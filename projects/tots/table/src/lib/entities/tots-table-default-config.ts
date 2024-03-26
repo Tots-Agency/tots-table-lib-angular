09db206a9fe5b9a2d1475ffc85534dfb5d5370fb
Alvaro <alvaro@MacBook-Air.local>
2024-03-26 17:36:39 -0300

    provided in root

--- projects/tots/table/src/lib/entities/tots-table-default-config.ts
+++ projects/tots/table/src/lib/entities/tots-table-default-config.ts
@@ -2,7 +2,9 @@ import { Injectable, InjectionToken } from "@angular/core";
 
 export const TOTS_TABLE_DEFAULT_CONFIG = new InjectionToken<TotsTableDefaultConfig>('tots_table_default_config');
 
-@Injectable()
+@Injectable({
+  providedIn: "root"
+})
 export class TotsTableDefaultConfig {
   messageNotFound? : string =  "No results found, please try with other search terms";
   loadingComponent? : any;
